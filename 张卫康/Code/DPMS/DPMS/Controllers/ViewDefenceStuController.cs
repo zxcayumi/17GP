babd1f3b22270d090be09ffd37f43c197c6098b5
maihedandexiaonvhai <79362972+maihedandexiaonvhai@users.noreply.github.com>
2021-04-16 17:01:14 +0800

    更新

---
+++ 张卫康/Code/DPMS/DPMS/Controllers/ViewDefenceStuController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DPMS.Controllers
+{
+  [ApiController]
+  [Route("ViewDefenceStu")]
+  public class ViewDefenceStuController
+  {
+  }
+}
---
+++ 张卫康/Code/DPMS/DPMS/Controllers/ViewStuResultController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DPMS.Controllers
+{
+  [ApiController]
+  [Route("ViewStuResult")]
+  public class ViewStuResultController
+  {
+  }
+}
